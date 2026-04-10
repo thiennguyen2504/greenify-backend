622ccd62a0f72a75429f32ff6bc1abc142c4c279
Nguyễn Minh Thiện <23521484@gm.uit.edu.vn>
2026-04-11 04:11:25 +0700

    chore: delete unused file

--- src/main/java/com/webdev/greenify/media/service/MediaService.java
+++
@@ -1,9 +0,0 @@
-package com.webdev.greenify.media.service;
-
-import com.webdev.greenify.media.dto.request.MediaUploadRequest;
-import com.webdev.greenify.media.dto.response.MediaUploadResponse;
-
-public interface MediaService {
-
-    MediaUploadResponse uploadMedia(MediaUploadRequest request);
-}
