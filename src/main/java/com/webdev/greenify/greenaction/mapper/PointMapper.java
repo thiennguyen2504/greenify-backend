79c13161d7b204f058de7b9acf8c23ad2031516d
Nguyễn Minh Thiện <23521484@gm.uit.edu.vn>
2026-04-15 10:36:51 +0700

    fix: syntax

--- src/main/java/com/webdev/greenify/greenaction/mapper/PointMapper.java
+++ src/main/java/com/webdev/greenify/greenaction/mapper/PointMapper.java
@@ -3,7 +3,7 @@ package com.webdev.greenify.greenaction.mapper;
 import com.webdev.greenify.greenaction.dto.response.PointHistoryResponse;
 import com.webdev.greenify.greenaction.entity.PointTransactionEntity;
 import org.mapstruct.Mapper;
-adimport org.mapstruct.Mapping;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PointMapper {
