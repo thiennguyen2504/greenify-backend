bd27cb6ea768a52e704628c002c713e2922186d4
Thinh Nguyen Phuc <nguyenphucthinh123pro@gmail.com>
2026-04-23 21:47:16 +0700

    fix: implement Serializable

--- src/main/java/com/webdev/greenify/analyst/dto/AnalystDashboardDTO.java
+++ src/main/java/com/webdev/greenify/analyst/dto/AnalystDashboardDTO.java
@@ -5,13 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AnalystDashboardDTO {
+public class AnalystDashboardDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private AnalystMetricDTO totalMetrics;
     private List<MonthlyMetricDTO> monthlyBreakdown;
 }
--- src/main/java/com/webdev/greenify/analyst/dto/NGOAnalystMetricDTO.java
+++ src/main/java/com/webdev/greenify/analyst/dto/NGOAnalystMetricDTO.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class NGOAnalystMetricDTO {
+public class NGOAnalystMetricDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private long totalEvents;
     private long totalParticipants;
     private double averageAttendanceRate;
